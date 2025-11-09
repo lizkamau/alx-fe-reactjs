6114b0798c5eab0970e1f0b89fb052e213f28a48
nyaruaKamau <nyaruakamau@gmail.com>
2025-11-09 16:24:42 +0300

    inline css

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
