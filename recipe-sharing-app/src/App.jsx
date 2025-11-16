1ba3f16d8de68cf619036821017429839d281be6
nyaruaKamau <nyaruakamau@gmail.com>
2025-11-16 22:22:18 +0300

    recipe app enhancing the recipe store using zustand

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Recipe Sharing App</h1>
+    <Router>
+      <div style={{ padding: "20px" }}>
+        <h1>Recipe Sharing App</h1>
 
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+        <Routes>
+          {/* Home route */}
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+
+          {/* Recipe details route */}
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
