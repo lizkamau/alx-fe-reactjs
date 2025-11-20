4f1ba785b105ec16dc7d0203c8ec3b57da323988
nyaruaKamau <nyaruakamau@gmail.com>
2025-11-20 22:10:15 +0300

    initial commit for the setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Start building your app...</p>
+    </div>
+  );
+}
+
+export default App;
