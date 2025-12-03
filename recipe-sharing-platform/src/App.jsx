105f709f2e7d0e875667451e38d051d1558df01d
nyaruaKamau <nyaruakamau@gmail.com>
2025-12-03 19:31:45 +0300

    initial commit for recipe sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-500">Tailwind is working!</h1>
+
+      <p className="mt-4 text-green-600">
+        If this text is green, Tailwind v4 is set up correctly.
+      </p>
+    </div>
+  );
+}
