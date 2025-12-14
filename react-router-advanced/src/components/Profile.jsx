2ab5f57a6ec9e2ce7e1225cc5df1c72738c6284b
nyaruaKamau <nyaruakamau@gmail.com>
2025-12-14 11:21:29 +0300

    updates on profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -3,12 +3,13 @@ import { Outlet, Link } from "react-router-dom";
 function Profile() {
   return (
     <div>
-      <h2>Profile</h2>
+      <h2>Profile Page</h2>
 
       <nav>
         <Link to="details">Details</Link> | <Link to="settings">Settings</Link>
       </nav>
 
+      {/* Nested routes will render here */}
       <Outlet />
     </div>
   );
